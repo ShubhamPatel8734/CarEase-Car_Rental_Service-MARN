8115a874e73e191a5121e209952f5a4f948adabc
Om Mehta <ommehta4920@gmail.com>
2024-08-29 10:46:02 +0530

    Pages Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom'
 //[Import local folders/files]
 // * Components
 import { Layout } from './components/index'
-import { Home, Contact } from './pages/index'
+import { Home, About, Cars, Contact, FAQ } from './pages/index'
 
 // * Stylesheet
 import './App.css'
@@ -19,7 +19,18 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout/>}>
             <Route index element={<Home />} />
+          </Route>
+          <Route path='/about' element={<Layout/>}>
+            <Route index element={<About />} />
+          </Route>
+          <Route path='/cars' element={<Layout/>}>
+            <Route index element={<Cars />} />
+          </Route>
+          <Route path='/contact' element={<Layout/>}>
             <Route index element={<Contact />} />
+          </Route>
+          <Route path='/faq' element={<Layout/>}>
+            <Route index element={<FAQ />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,14 @@
 // Home Page
 export { default as Home } from '../pages/Home.jsx'
 
+// Abous Us Page
+export { default as About } from '../pages/About.jsx'
+
+// Cars Page
+export { default as Cars } from '../pages/Cars.jsx'
+
 // Contact Page
 export { default as Contact } from '../pages/Contact.jsx'
+
+// FAQ Page
+export { default as FAQ } from '../pages/FAQ.jsx'
