efec0472e6a86332f545a9e04290a8a069002a93
Om Mehta <ommehta4920@gmail.com>
2024-09-05 08:45:25 +0530

    FAQ Update in Design

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -2,68 +2,57 @@ import React, { useState } from "react";
 import { FaAngleRight, FaAngleDown } from "react-icons/fa6";
 import "../pages/FAQ.css";
 
+const data = [
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+  {title: 'Age and Responsibility', desc:'Driver must be 25+ years old to drive economy, luxury cars and supercars.'},
+]
+
 function FAQ() {
-  // const faqData = [
-  //   {
-  //     desc: "Driver must be 25+ years old to drive economy, luxury cars and supercars.",
-  //   },
-  //   {
-  //     desc: "Driver must be 25+ years old to drive economy, luxury cars and supercars.",
-  //   },
-  //   {
-  //     desc: "Driver must be 25+ years old to drive economy, luxury cars and supercars.",
-  //   },
-  // ];
 
-  // const FAQItem = ({ faq, index, toggleFAQ, isOpen }) => {
-  //   return (
-  //     <div className="faq-item">
-  //       <div className="faq-question" onClick={() => toggleFAQ(index)}>
-  //         {faq.question}
-  //       </div>
-  //       {isOpen && <div className="faq-answer">{faq.answer}</div>}
-  //     </div>
-  //   );
-  // };
+  const [ selected, setSelected ] = useState(null);
 
-  // const FAQ = () => {
-  //   const [openIndex, setOpenIndex] = useState(null);
+  const toggle = (i) => {
+    if(selected == i){
+      return setSelected(null);
+    }
+    setSelected(i);
+  }
 
-  //   const toogleFAQ = (index) => {
-  //     setOpenIndex(openIndex === index ? null : index);
-  //   };
-
-    return (
-      <div>
-        <div className="faq-title">
-          <div className="faq-title-text">
-            <h4>Frequently Asked Questions</h4>
-            <h1>Popular Questions</h1>
-          </div>
-        </div>
-        <div className="faq-questions">
-          <div className="faq-left">
-            {/* <div className="faq-section">
-              <h2>Frequently Asked Questions</h2>
-                {faqData.map((faq, index) => (
-                  <FAQItem
-                    key={index}
-                    faq={faq}
-                    index={index}
-                    isOpen={openIndex === index}
-                    toggleFAQ={toggleFAQ}
-                  />
-                ))}
-              </div> */}
-          </div>
-          {/* <div className="faq-middle">
-            <img src="../images/faq2.png" alt="faq-image"></img>
-          </div>
-          <div className="faq-right"></div> */}
+  return (
+    <div>
+      <div className="faq-title">
+        <div className="faq-title-text">
+          <h4>Frequently Asked Questions</h4>
+          <h1>Popular Questions</h1>
         </div>
       </div>
-    );
-  };
-// }
+      <div className="faq-questions">
+        <div className="faq-left">
+          <div className="accordion">
+            {data.map( (faq, i) => (
+              <div className="question-container">
+                <div className="question-title" onClick={() => toggle(i)}>
+                  <h2>{faq.title}</h2>
+                  <span>{selected === i ? <FaAngleDown /> : <FaAngleRight />}</span>
+                </div>
+                <div className="{selected === i ? 'question-desc show' : 'question-desc'}">
+                  {faq.desc}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* <div className="faq-middle">
+          <img src="../images/faq2.png" alt="faq-image"></img>
+        </div> */}
+        <div className="faq-right"></div>
+      </div>
+    </div>
+  );
+}
 
 export default FAQ;
