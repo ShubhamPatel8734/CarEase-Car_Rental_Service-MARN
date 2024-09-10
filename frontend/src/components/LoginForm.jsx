e90741d415b5dd75e1e5c9072ac5c2ca3abd104a
Om Mehta <ommehta4920@gmail.com>
2024-09-10 18:28:02 +0530

    Login & Register Update

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -8,8 +8,8 @@ const LoginForm = ({ onClose }) => {
       <div className="popup-login-content">
         <h2 className='login-title'><span>S</span>ign <span>I</span>n</h2>
         <form className='login-form'>
-          <label htmlFor="username" className='login-label'>Username:</label>
-          <input type="text" id="username" name="username" placeholder='Enter Username' className='login-textbox' required />
+          <label htmlFor="email-id" className='login-label'>Email ID:</label>
+          <input type="email" id="email" name="email" placeholder='Enter Email ID' className='login-textbox' required />
           <label htmlFor="password" className='login-label'>Password:</label>
           <input type="password" id="password" name="password" placeholder='Enter Password' className='login-textbox' required />
           <button type="submit" className='login-btn'>Login</button>
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -8,12 +8,16 @@ const RegisterForm = ({ onClose }) => {
       <div className="popup-register-content">
         <h2 className='register-title'><span>S</span>ign <span>U</span>p</h2>
         <form className='register-form'>
-          <label htmlFor="email" className='register-label'>Email</label>
-          <input type="email" id="email" name="email" placeholder='Enter Email ID' className='register-textbox' required />
           <label htmlFor="username" className='register-label'>Username</label>
           <input type="text" id="username" name="username" placeholder='Enter Username' className='register-textbox' required />
+          <label htmlFor="email" className='register-label'>Email</label>
+          <input type="email" id="email" name="email" placeholder='Enter Email ID' className='register-textbox' required />
+          <label htmlFor="phone" className='register-label'>Phone No</label>
+          <input type="tel" id="phone" name="phone" placeholder='Enter Phone No' className='register-textbox' required />
           <label htmlFor="password" className='register-label'>Password</label>
           <input type="password" id="password" name="password" placeholder='Enter Password' className='register-textbox' required />
+          <label htmlFor="confirm-password" className='register-label'>Confirm Password</label>
+          <input type="password" id="confirm-password" name="confirm-password" placeholder='Enter Password Again' className='register-textbox' required />
           <button type="submit" className='register-btn'>Register</button>
         </form>
       </div>
