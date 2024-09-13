a9fd4003062a7881c3cdb4a27a768c3e32f2b515
Om Mehta <ommehta4920@gmail.com>
2024-09-13 20:18:50 +0530

    Admin Login

--- frontend/src/admin-pages/AdminLogin.jsx
+++ frontend/src/admin-pages/AdminLogin.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../admin-pages/AdminLogin.css'
 
 const AdminLogin = () => {
   return (
-    <div>Admin Login</div>
+    <div>
+      <div className='admin-login-container'>
+        <h1><span>C</span>ar<span>E</span>ase - <span>S</span>ign <span>I</span>n</h1>
+        <div className='admin-login-content'>
+          <form className='admin-login-form'>
+            <label htmlFor="email-id" className='admin-login-label'>Email ID:</label>
+            <input type="email" id="email" name="email" placeholder='Enter Email ID' className='admin-login-textbox' required />
+            <label htmlFor="password" className='admin-login-label'>Password:</label>
+            <input type="password" id="password" name="password" placeholder='Enter Password' className='admin-login-textbox' required />
+            <button type="submit" className='admin-login-btn'>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
