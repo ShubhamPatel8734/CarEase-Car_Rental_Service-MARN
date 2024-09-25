d912a4da24ee54bad6c7553ea20033ef86f21135
Om Mehta <ommehta4920@gmail.com>
2024-09-25 18:04:11 +0530

    Visitor's Car Page Completed

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,7 +1,117 @@
-import React from 'react'
-import '../pages/Cars.css'
+import React, { useState } from "react";
+import { FaCar, FaSuitcase  } from "react-icons/fa";
+import { TbUser, TbManualGearbox } from "react-icons/tb";
+import "../pages/Cars.css";
+
+const ItemCard = ({ item }) => {
+  return (
+    <div className="carcard">
+      <div className="carcard-content">
+        <div className="carcard-img">
+          <img src="../images/cars.jpg" alt={item.name}></img>
+        </div>
+        <div className="carcard-desc">
+          <div className="carcard-title">
+            <h3>{item.name}</h3>
+          </div>
+          <div className="carcard-info">
+            <h4><span><FaCar/></span> Car Type</h4>
+            <p>{item.category}</p>
+          </div>
+          <div className="carcard-info">
+            <h4><span><TbUser/></span> Passengers</h4>
+            <p>{item.passenger}</p>
+          </div>
+          <div className="carcard-info">
+            <h4><span><TbManualGearbox/></span> Transmission</h4>
+            <p>{item.transmission}</p>
+          </div>
+          <div className="carcard-info">
+            <h4><span><FaSuitcase /></span> Luggage</h4>
+            <p>{item.luggage}</p>
+          </div>
+          <div className="carcard-footer">
+            <h3>{item.rent}<span> /day</span></h3>
+            <button className="carcard-rentbtn">Rent Now</button>
+          </div>
+        </div>
+      </div>
+        
+    </div>
+  );
+};
 
 const Cars = () => {
+  const [selectedFilter, setSelectedFilter] = useState("All");
+
+  const filters = ["All", "Car1", "Car2", "Car3", "Car4"];
+
+  const items = [
+    {
+      id: 1,
+      name: "Aston Martin DBX",
+      category: 'Car1',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+    {
+      id: 2,
+      name: "Aston Martin DBX",
+      category: 'Car2',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+    {
+      id: 3,
+      name: "Aston Martin DBX",
+      category: 'Car1',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+    {
+      id: 4,
+      name: "Aston Martin DBX",
+      category: 'Car3',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+    {
+      id: 5,
+      name: "Aston Martin DBX",
+      category: 'Car4',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+    {
+      id: 6,
+      name: "Aston Martin DBX",
+      category: 'Car1',
+      passenger: '5',
+      transmission: 'Auto',
+      luggage: '4 bags',
+      rent: '2000',
+    },
+  ];
+
+  const handleFilterChange = (filter) => {
+    setSelectedFilter(filter);
+  };
+
+  const filteredItems =
+    selectedFilter === "All"
+      ? items
+      : items.filter((item) => item.category === selectedFilter);
+
   return (
     <div>
       <div className="cars-title">
@@ -10,8 +120,32 @@ const Cars = () => {
           <h1>Select A Car</h1>
         </div>
       </div>
+      <div className="cars-body">
+        <div className="filter-links-container">
+          <div className="filter-links">
+            <div className="links-btn">
+              {filters.map((filter, index) => (
+                <button
+                  key={index}
+                  className={selectedFilter === filter ? 'active' : ''}
+                  onClick={() => handleFilterChange(filter)}
+                >
+                  {filter}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="carcard-list-container">
+          <div className="carcard-list">
+              {filteredItems.map((item) => (
+                <ItemCard key={item.id} item={item}/>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cars
+export default Cars;
