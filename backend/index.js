a1e4f1f42a97c6ab610423342143ac2442a440c7
OmMehta07 <64518918+ommehta4920@users.noreply.github.com>
2024-10-18 18:21:02 +0530

    Backend Added

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import cors from 'cors';
+dotenv.config();
+import { UserRouter } from './routes/user.js';
+const app=express();
+app.use(express.json());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    method:["POST","GET"],
+    credentials: true
+}));
+app.use('/user', UserRouter);
+mongoose.connect('mongodb://127.0.0.1:27017/Carease')
+app.listen(process.env.PORT,()=>{
+    console.log("Server is listening on port 3000.");
+})
